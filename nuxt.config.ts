aed2822722b59df59235ffa4aaba7f0461c8ca16
Hendrik Schmitz <hendrik.schmitz@aixigo.com>
2023-11-22 11:54:04 +0100

    Add antfu eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['primevue/resources/themes/lara-dark-green/theme.css'],
   modules: [
-    'nuxt-primevue'
+    'nuxt-primevue',
   ],
   primevue: {
     components: {
-        prefix: 'Prime'
+      prefix: 'Prime',
     },
   },
 })
